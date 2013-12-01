283396bc29dbf9338ed556721d172b5f4972b952
Peter Nguyen <peter@likipe.se>
2013-12-01 06:35:57 +0100

    Added default options to enable HTML5 mode for links and CORS

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -10,11 +10,23 @@ define([
 
 		// Declare app level module which depends on filters, and services
 
-		return angular.module('app', [
+		var App = angular.module('app', [
 			'ngRoute',
 			'app.controllers',
 			'app.filters',
 			'app.services',
 			'app.directives'
 		]);
+
+		App.config(['$locationProvider', '$httpProvider', function($locationProvider, $httpProvider) {
+			$locationProvider.html5Mode(true);
+			$locationProvider.hashPrefix('!');
+
+			// Allow CORS
+			$httpProvider.defaults.useXDomain      = true;
+			$httpProvider.defaults.withCredentials = true;
+			delete $httpProvider.defaults.headers.common['X-Requested-With'];
+		}]);
+
+		return App;
 });
